96f8af19ca4d510c22fcdb3dae13b6e549fb038e
Les Pounder <lespounder@gmail.com>
2015-10-03 14:26:32 +0100

    Working with audio and menu

--- stepper.py
+++ stepper.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python3.4
 import RPi.GPIO as GPIO
 import time, random
 import easygui as eg
+import pygame
 
 delay = 0.01
 
@@ -17,6 +19,12 @@ outputs = [IN1,IN2,IN3,IN4]
 for pin in outputs:
     GPIO.setup(pin, GPIO.OUT)
 GPIO.setup(button, GPIO.IN, GPIO.PUD_UP)
+
+def audio(file):
+    pygame.mixer.init()
+    sound = pygame.mixer.music.load(file)
+    pygame.mixer.music.play(1)
+
 
 def cw(steps,delay):
     for i in range(steps):
@@ -66,34 +74,44 @@ def ccw(steps,delay):
         GPIO.output(IN1, False)
         time.sleep(delay)
 
+eg.msgbox(title="Welcome to the Python Quiz",image="./python.gif",msg="So you think you know Python?")
 while True:
     if GPIO.input(button) == False:
+        audio("./tilburg.mp3")
         steps = random.randint(1,512)
         cw(steps,delay)
         if steps >0 and steps < 128:
             answer = eg.choicebox(title="Question",msg="Which variable is storing a string?",choices=("a='Hello World'","b = 5","c = 2.0"))
             if answer == "a='Hello World'":
-                eg.msgbox(title="CORRECT",msg="RIGHT ANSWER")
+                audio("./correct.mp3")
+                eg.msgbox(title="CORRECT",image="./tick.gif",msg="RIGHT ANSWER")
             else:
-                eg.msgbox(title="INCORRECT",msg="WRONG ANSWER")
+                audio("./wrong.mp3")
+                eg.msgbox(title="INCORRECT",image="./cross.gif",msg="WRONG ANSWER")
         elif steps >128 and steps < 256:
             answer = eg.choicebox(title="Question",msg="How can I add an item to a list?",choices=("append","pop","add"))
             if answer == "append":
-                eg.msgbox(title="CORRECT",msg="RIGHT ANSWER")
+                audio("./correct.mp3")
+                eg.msgbox(title="CORRECT",image="./tick.gif",msg="RIGHT ANSWER")
             else:
-                eg.msgbox(title="INCORRECT",msg="WRONG ANSWER")            
+                audio("./wrong.mp3")
+                eg.msgbox(title="INCORRECT",image="./cross.gif",msg="WRONG ANSWER")            
         elif steps >256 and steps < 384:
             answer = eg.choicebox(title="Question",msg="Which loop will go on forever?",choices=("While True","For","While False"))
             if answer == "While True":
-                eg.msgbox(title="CORRECT",msg="RIGHT ANSWER")
+                audio("./correct.mp3")
+                eg.msgbox(title="CORRECT",image="./tick.gif",msg="RIGHT ANSWER")
             else:
-                eg.msgbox(title="INCORRECT",msg="WRONG ANSWER")
+                audio("./wrong.mp3")
+                eg.msgbox(title="INCORRECT",image="./cross.gif",msg="WRONG ANSWER")
         elif steps > 384 and steps < 512:
                 answer = eg.choicebox(title="Question",msg="Which data type is best used for precise numerical data?",choices=("float","string","integer"))
                 if answer == "float":
-                    eg.msgbox(title="CORRECT",msg="RIGHT ANSWER")
+                    audio("./correct.mp3")
+                    eg.msgbox(title="CORRECT",image="./tick.gif",msg="RIGHT ANSWER")
                 else:
-                    eg.msgbox(title="INCORRECT",msg="WRONG ANSWER")
+                    audio("./wrong.mp3")
+                    eg.msgbox(title="INCORRECT",image="./cross.gif",msg="WRONG ANSWER")
     else:
         print("Waiting")
         time.sleep(0.1)
